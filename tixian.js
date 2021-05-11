b3d8d11981256ac58a558b6efc4f21289298c0e0
ck1397 <39193522+ck1397@users.noreply.github.com>
2021-05-11 13:54:50 +0800

    Update tixian.js

--- tixian.js
+++ tixian.js
@@ -69,7 +69,6 @@ $.userName = '';
     if ($.currentCookie) {
       $.userName =  decodeURIComponent($.currentCookie.match(/pt_pin=(.+?);/) && $.currentCookie.match(/pt_pin=(.+?);/)[1]);
       $.log(`\n开始【京东账号${i + 1}】${$.userName}`);
-      
       await cashOut();
     }
   }
